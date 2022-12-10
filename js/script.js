7e9efdbf07bfb52f897ed8b2003eed37fff0ade2
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2022-12-10 16:00:06 +0100

    clean js and html code

--- js/script.js
+++ js/script.js
@@ -1,46 +1,27 @@
 'use strict';
-/*
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-});
-*/
 
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!');
-    console.log(event);
 
-    /*  [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for(let activeLink of activeLinks){
         activeLink.classList.remove('active');
     }
 
-    /* [DONE] add class 'active' to the clicked link */
-    console.log('clickedElement:', clickedElement);
     clickedElement.classList.add('active');
 
-    /* [Done] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.post.active');
 
     for(let activeArticle of activeArticles){
         activeArticle.classList.remove('active');
     }
 
-    /* get 'href' attribute from the clicked link */
     const linkAttribute = clickedElement.getAttribute('href');
-    console.log(linkAttribute);
 
-    /* find the correct article using the selector (value of 'href' attribute) */
     const targetArticle = document.querySelector(linkAttribute);
 
-    console.log(targetArticle);
-
-    /* add class 'active' to the correct article */
-    console.log('clickedElement:', targetArticle);
     targetArticle.classList.add('active');
 }
 
