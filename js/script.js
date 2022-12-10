82c8f9bb8e75c80fc3b3ce84c08e507b9d0ef1eb
Dariusz Wasilewski <dariusz.wasilewski.86@gmail.com>
2022-12-10 18:20:27 +0100

    add function js (version1)

--- js/script.js
+++ js/script.js
@@ -30,3 +30,28 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
     link.addEventListener('click', titleClickHandler);
 }
+
+function generateTitleLinks (){
+
+    /* [DONE] remove content of titleList */
+    const clearTitleList = function(){
+        document.querySelector('.titles').innerHTML = '';
+    }
+
+    clearTitleList();
+
+    /* for each article */
+    const articles = document.querySelectorAll('.post');
+
+    for(let article of articles){};
+
+        /* get the article id */
+    const articleId = article.getAttribute('id');
+
+        /* find the title element */
+
+        /* get the title from the title element */
+
+        /* create HTML of the link */
+}
+generateTitleLinks();
